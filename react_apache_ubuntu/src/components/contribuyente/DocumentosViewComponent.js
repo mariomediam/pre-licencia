cd76438dd79188263a98dceda8a66954e5377291
MARIO A. MEDINA MARQUEZ <mmedina@MUNIPIURA.GOB.PE>
2022-11-28 15:21:37 -0500

    chore: Buscar contribuyente pestania otros

---
+++ react_apache_ubuntu/src/components/contribuyente/DocumentosViewComponent.js
@@ -0,0 +1,86 @@
+import { useState, useEffect } from "react";
+import {
+  obtenerContribuyenteDocumento,
+  obtenerContribuyenteTelefono,
+  obtenerContribuyenteDirElect,
+  obtenerContribuyenteNacion,
+} from "../../services/contribuyenteService";
+
+export const DocumentosViewComponent = ({
+  codigoContrib,
+  codigoContribSelecc,
+}) => {
+  const [documentos, setDocumentos] = useState([]);
+  const [telefonos, setTelefonos] = useState([]);
+  const [dirElect, setDirElect] = useState([]);
+  const [nacion, setNacion] = useState([]);
+
+  const verDocumentos = async () => {
+    if (
+      codigoContrib &&
+      codigoContribSelecc &&
+      codigoContrib === codigoContribSelecc
+    ) {
+      console.log("Lee otros");
+      const documentosTmp = await obtenerContribuyenteDocumento(
+        codigoContribSelecc
+      );
+      setDocumentos(documentosTmp);
+
+      const telefonosTmp = await obtenerContribuyenteTelefono(
+        codigoContribSelecc
+      );
+      setTelefonos(telefonosTmp);
+
+      const dirElectTmp = await obtenerContribuyenteDirElect(
+        codigoContribSelecc
+      );
+      setDirElect(dirElectTmp);
+
+      const naciontTmp = await obtenerContribuyenteNacion(codigoContribSelecc);
+      setNacion(naciontTmp);
+    }
+  };
+
+  useEffect(() => {
+    verDocumentos();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [codigoContribSelecc]);
+
+  return (
+    <div className="ps-2">
+      <p className="text-muted pb-0 mb-0 mt-0">
+        <small className="mb-0">Documentos</small>
+      </p>
+
+      {documentos.map((docum, i) => (
+        <div key={i}>
+          <small>{docum.Descripción}</small> {docum.Número}
+        </div>
+      ))}
+
+      <p className="text-muted pb-0 mb-0 mt-2">
+        <small className="mb-0">Teléfonos</small>
+      </p>
+      {telefonos.map((telefono, i) => (
+        <div key={i}>
+          <small>{telefono.TipTel}</small> {telefono.Número}
+        </div>
+      ))}
+
+      <p className="text-muted pb-0 mb-0 mt-2">
+        <small className="mb-0">Dirección electrónica</small>
+      </p>
+      {dirElect.map((dirElect, i) => (
+        <div key={i}>{dirElect.Dirección_Electrónica}</div>
+      ))}
+
+      <p className="text-muted pb-0 mb-0 mt-2">
+        <small className="mb-0">Nacionalidad</small>
+      </p>
+      {nacion.map((nac, i) => (
+        <div key={i}>{nac.Gentilicio}</div>
+      ))}
+    </div>
+  );
+};
--- react_apache_ubuntu/src/services/contribuyenteService.js
+++ react_apache_ubuntu/src/services/contribuyenteService.js
@@ -1,6 +1,6 @@
 import UseAxios from "../utils/useAxios";
 
-// const URL = `${process.env.REACT_APP_API}/contribuyente`;
+const URL = `${process.env.REACT_APP_API}/contribuyente`;
 
 const obtenerContribuyentePagination = async (URLBuscarContribuyente) => {
   
@@ -15,6 +15,75 @@ const obtenerContribuyentePagination = async (URLBuscarContribuyente) => {
     }
   };  
 
+const obtenerContribuyenteDocumento = async (codigoContrib) => {
+  try {
+    let api = UseAxios();
+
+    let URLContribDocumento = `${URL}/consultar-contribuyente-documento?codigo=${codigoContrib}`;
+
+    let {
+      data: { content },
+    } = await api.get(`${URLContribDocumento}`);
+
+    return content;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const obtenerContribuyenteTelefono = async (codigoContrib) => {
+  try {
+    let api = UseAxios();
+
+    let URLContribTelefono = `${URL}/consultar-contribuyente-telefono?codigo=${codigoContrib}`;
+
+    let {
+      data: { content },
+    } = await api.get(`${URLContribTelefono}`);
+
+    return content;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const obtenerContribuyenteDirElect = async (codigoContrib) => {
+  try {
+    let api = UseAxios();
+
+    let URLContribDirElect = `${URL}/consultar-contribuyente-direlect?codigo=${codigoContrib}`;
+
+    let {
+      data: { content },
+    } = await api.get(`${URLContribDirElect}`);
+
+    return content;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const obtenerContribuyenteNacion = async (codigoContrib) => {
+  try {
+    let api = UseAxios();
+
+    let URLContribNacion = `${URL}/consultar-contribuyente-nacion?codigo=${codigoContrib}`;
+
+    let {
+      data: { content },
+    } = await api.get(`${URLContribNacion}`);
+
+    return content;
+  } catch (error) {
+    throw error;
+  }
+};
+
   export {
     obtenerContribuyentePagination,
+    obtenerContribuyenteDocumento,
+    obtenerContribuyenteTelefono,
+    obtenerContribuyenteDirElect,
+    obtenerContribuyenteNacion
+
   };
