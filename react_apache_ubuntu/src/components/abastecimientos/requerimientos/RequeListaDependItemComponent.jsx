18d17d6d673f6d202f1e6bf7b3045a205ded4115
Your Name <you@example.com>
2024-02-08 15:05:44 -0500

    Split button gestiona requerimientos

--- react_apache_ubuntu/src/components/abastecimientos/requerimientos/RequeListaDependItemComponent.jsx
+++ react_apache_ubuntu/src/components/abastecimientos/requerimientos/RequeListaDependItemComponent.jsx
@@ -1,4 +1,5 @@
 import { transformarFecha } from "../../../utils/varios";
+import { RequeListaOptionSplitComponent } from "./RequeListaOptionSplitComponent";
 
 const classBadge = {
   1: "badge bg-secondary",
@@ -8,7 +9,9 @@ const classBadge = {
 
 export const RequeListaDependItemComponent = ({ requerimiento }) => {
   const {
+    C_anipre,
     C_reque,
+    C_biesertipo,
     F_reque_estado,
     tipo_reque,
     D_reque_fecha,
@@ -23,14 +26,13 @@ export const RequeListaDependItemComponent = ({ requerimiento }) => {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   });
+
   return (
     <>
       <td className="align-middle pe-3 ">
-        <div>{C_reque}
-        </div>
-        <div><small className="text-muted">
-           {tipo_reque}
-          </small>
+        <div>{C_reque}</div>
+        <div>
+          <small className="text-muted">{tipo_reque}</small>
         </div>
         <div>
           <small className="text-muted">
@@ -39,40 +41,22 @@ export const RequeListaDependItemComponent = ({ requerimiento }) => {
         </div>
         <div>
           <span className={classBadge[F_reque_estado]}>
-            <small>{F_reque_estado === "2" ? `PRE COMPROMISO ${parseInt(C_exp, 10)}` : N_ESTADO_NOMBRE}</small>
+            <small>
+              {F_reque_estado === "2"
+                ? `PRE COMPROMISO ${parseInt(C_exp, 10)}`
+                : N_ESTADO_NOMBRE}
+            </small>
           </span>
         </div>
         <div></div>
       </td>
-      <td className="align-middle"><p className=" max-five-lines">{T_reque_obs}</p></td>
+      <td className="align-middle">
+        <p className=" max-five-lines">{T_reque_obs}</p>
+      </td>
       <td className="text-end align-middle ">{formattedRequeTotal}</td>
       <td className="text-center align-middle ">
-        {/* <Button variant="outline-primary" onClick={onClicVerEnvios}>
-        {" "}
-        Ver envios{" "}
-      </Button>{" "} */}
+        <RequeListaOptionSplitComponent C_anipre={C_anipre} C_reque={C_reque} C_biesertipo={C_biesertipo} F_reque_estado={F_reque_estado}/>
       </td>
-      {/* <td
-      className={`text-center align-middle m-1 p-1 ${
-        actual_envio !== f_plani_envio &&
-        "animate__animated animate__zoomIn"
-      }`}
-    >
-      <CellEnviarBoleta
-        d_ano={d_ano}
-        d_mes={d_mes}
-        c_tippla_id={c_tippla_id}
-        c_plani_nro={c_plani_nro}
-        actual_envio={actual_envio}
-        actual_user={actual_user}
-        actual_datetime={actual_datetime}
-        actual_carpeta={actual_carpeta}
-        n_tippla_nombre = {n_tippla_nombre}
-        setEstadoPlanillaBoleta={setEstadoPlanillaBoleta}
-      />
-
-    </td> */}
-    
     </>
   );
 };
---
+++ react_apache_ubuntu/src/components/abastecimientos/requerimientos/RequeListaOptionSplitComponent.jsx
@@ -0,0 +1,132 @@
+export const RequeListaOptionSplitComponent = ({
+  C_anipre,
+  C_reque,
+  C_biesertipo,
+  F_reque_estado,
+}) => {
+  return (
+    <div className="dropdown">
+      <button
+        className="btn"
+        type="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        <svg
+          width="12"
+          height="14"
+          fill="currentColor"
+          class="bi bi-three-dots-vertical"
+          viewBox="0 0 16 16"
+        >
+          <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+        </svg>
+      </button>
+      <ul className="dropdown-menu  ">
+        {F_reque_estado === "1" && (
+          <li>
+            <a className="d-flex align-items-center dropdown-item" href=".">
+              <img
+                src="/images/file-dollar.svg"
+                className="me-1"
+                thumbnail
+                alt="Precomprometer requerimiento"
+              />
+              Precomprometer
+            </a>
+          </li>
+        )}
+
+        {F_reque_estado === "2" && (
+          <li>
+            <a className="d-flex align-items-center dropdown-item" href=".">
+              <img
+                src="/images/printer.svg"
+                className="me-1"
+                thumbnail
+                alt="Imprimir requerimiento"
+              />
+              Imprimir
+            </a>
+          </li>
+        )}
+
+        <li>
+          <a className="d-flex align-items-center dropdown-item" href=".">
+            <img
+              src="/images/eye.svg"
+              className="me-1"
+              thumbnail
+              alt="Ver requerimiento"
+            />
+            Ver
+          </a>
+        </li>
+
+        {F_reque_estado === "1" && (
+          <>
+            <li>
+              <hr class="dropdown-divider" />
+            </li>
+            <li className="text-primary">
+              <a
+                className="d-flex align-items-center text-primary dropdown-item"
+                href="."
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-pencil me-1"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
+                  <path d="M13.5 6.5l4 4" />
+                </svg>
+                Modificar
+              </a>
+            </li>
+          </>
+        )}
+
+        {F_reque_estado === "1" && (
+          <>
+            <li>
+              <hr class="dropdown-divider" />
+            </li>
+            <li className="text-danger">
+              <a
+                className="d-flex align-items-center text-danger dropdown-item"
+                href="."
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-square-x me-1"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M3 5a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-14z" />
+                  <path d="M9 9l6 6m0 -6l-6 6" />
+                </svg>
+                Anular
+              </a>
+            </li>
+          </>
+        )}
+      </ul>
+    </div>
+  );
+};
