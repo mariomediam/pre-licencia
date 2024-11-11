4249d9a1d693e0a9c13bc86933d79daf1eff38e1
Mario Medina <mariomedinam@gmail.com>
2024-11-11 11:21:44 -0500

    Show SIAF searches

--- react_apache_ubuntu/src/components/tesorero/reports/filters/DetailedExecutionSIAF.jsx
+++ react_apache_ubuntu/src/components/tesorero/reports/filters/DetailedExecutionSIAF.jsx
@@ -1,7 +1,80 @@
+import { Table } from "react-bootstrap";
+import { DetailedExecutionSIAFItem } from "./DetailedExecutionSIAFItem";
+
 export const DetailedExecutionSIAF = ({ data}) => {
 
-    console.log("data siaf", data)
+
   return (
-    <div>{JSON.stringify(data)}</div>
+    <div style={{ border: "1px solid lightgrey", minWidth: "992px" }}>
+      <Table
+        size="sm"
+        hover
+        responsive
+        className="caption-top mb-1 animate__animated animate__fadeIn animate__faster"
+      >
+        <thead>
+          <tr className="color-header2 text-white">
+            <th className="align-middle m-0">
+              <small>Expediente</small>
+            </th>
+            <th className="align-middle m-0" title="Ciclo / Fase">
+              <small>C / F</small>
+            </th>
+            <th className=" align-middle m-0 p-0" title="Rubro / Tipo de recurso">
+              <small>Rb / Tr</small>
+            </th>
+            <th className=" align-middle m-0 p-0">
+              <small>Documento</small>
+            </th>
+            <th className=" align-middle m-0 p-0">
+              <small>Presupuestal</small>
+            </th>
+            <th className=" align-middle m-0 p-0">
+              <small>Proveedor</small>
+            </th>
+            <th className=" align-middle m-0 p-0">
+              <small>Importe</small>
+            </th>
+            <th className=" align-middle m-0 p-0">
+              <small>Glosa</small>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((record, index) => (
+            <DetailedExecutionSIAFItem key={index} record={record} />
+          ))}
+
+          {/* {listTributoArchivo.map(
+              (
+                {
+                  C_Archivo,
+                  C_TipOpe,
+                  M_Archivo_Anio,
+                  M_Archivo_Mes,
+                  D_Archivo_FecDig,
+                  C_Usuari_Login,
+                  N_Archivo_PC,
+                },
+                i
+              ) => (
+                <tr key={C_Archivo}>
+                  <TributoArchivoListarItemComponent
+                    {...{
+                      C_Archivo,
+                      C_TipOpe,
+                      M_Archivo_Anio,
+                      M_Archivo_Mes,
+                      D_Archivo_FecDig,
+                      C_Usuari_Login,
+                      N_Archivo_PC,
+                    }} fetchTributoPeriodosDisponibles = {fetchTributoPeriodosDisponibles}  fetchTributoArchivo = {fetchTributoArchivo} NTipOpe={NTipOpe}
+                  />
+                </tr>
+              )
+            )} */}
+        </tbody>
+      </Table>
+    </div>
   )
 }
---
+++ react_apache_ubuntu/src/components/tesorero/reports/filters/DetailedExecutionSIAFItem.jsx
@@ -0,0 +1,129 @@
+import { useEffect, useState, useRef } from "react";
+import { formatNumber, transformarFecha } from "../../../../utils/varios";
+
+export const DetailedExecutionSIAFItem = ({ record }) => {
+  const [isTruncated, setIsTruncated] = useState(false);
+  const [viewMore, setViewMore] = useState(false);
+  const [viewMoreText, setViewMoreText] = useState("Ver más");
+
+  const {
+    // sec_ejec,
+    ano_eje,
+    expediente,
+    fecha_doc,
+    ciclo,
+    fase,
+    // secuencia,
+    // CORRELATIVO,
+    cod_doc,
+    num_doc,
+    banco,
+    cta_cte,
+    ano_cta_cte,
+    sec_func,
+    monto,
+    // ID_CLASIFICADOR,
+    fuente_financ,
+    tipo_recurso,
+    clasificador,
+    tipo_operacion,
+    ruc,
+    NOMBRE,
+    glosa,
+    certificado,
+    // certificado_secuencia,
+  } = record;
+
+  const glosaRef = useRef(null);
+
+  useEffect(() => {
+    const glosa = glosaRef.current;
+
+    const isOverflowing = glosa.scrollHeight > glosa.clientHeight;
+
+    if (isOverflowing) {
+      setIsTruncated(true);
+    } else {
+      setIsTruncated(false);
+    }
+  }, []);
+
+  const toggleViewMore = () => {
+    setViewMore(!viewMore);
+    setViewMoreText(viewMore ? "Ver más" : "Ver menos");
+  };
+
+  return (
+    <tr style={{ fontSize: "12px" }}>
+      <td>
+        <div>
+          <p className="my-0 py-0">
+            {expediente}-{ano_eje}
+          </p>
+          <p className="my-0 py-0">
+            <small className="text-muted">Fecha:</small>{" "}
+            {transformarFecha(fecha_doc).substring(0, 10)}
+          </p>
+          <p className="my-0 py-0">
+            <small className="text-muted">T.O:</small> {tipo_operacion}
+          </p>
+          <p className="my-0 py-0">
+            <small className="text-muted">Certif:</small> {certificado}
+          </p>
+        </div>
+      </td>
+      <td className="">{`${ciclo} / ${fase}`}</td>
+      <td className="">{`${fuente_financ} / ${tipo_recurso}`}</td>
+      <td
+        className=""
+        style={{ maxWidth: "150px" }}
+      >{`${cod_doc} ${num_doc}`}</td>
+      <td className="">
+        <div>
+          <p className="my-0 py-0">
+            <small className="text-muted">Sec.Func: </small>
+            {sec_func}
+          </p>
+          <p className="my-0 py-0">
+            <small className="text-muted">Clasif: </small>
+            {clasificador}
+          </p>
+          {ano_cta_cte && (
+            <p className="my-0 py-0">
+              <small className="text-muted">Cuenta:</small> {`${ano_cta_cte}-${banco}-${cta_cte}`}
+            </p>
+          )}
+        </div>
+      </td>
+
+      <td className="">
+        <div>
+          <p className="my-0 py-0">{NOMBRE}</p>
+          <p className="my-0 py-0">
+            <small className="text-muted">Código: </small>
+            {ruc}
+          </p>
+        </div>
+      </td>
+      <td className="">{formatNumber(monto)}</td>
+      <td className="">
+        <p
+          className={`py-0 my-0 ${viewMore ? "" : "max-five-lines"}`}
+          style={{ maxWidth: "220px" }}
+          ref={glosaRef}
+        >
+          {glosa}
+        </p>
+        {isTruncated && (
+          <button
+            className="btn btn-link p-0 m-0"
+            onClick={toggleViewMore}
+            style={{ fontSize: "12px" }}
+          >
+            {viewMoreText}
+          </button>
+        )}
+      </td>
+    </tr>
+  )
+};
