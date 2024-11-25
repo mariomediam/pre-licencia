d004e3b81643b297d63a822ea9ca5fa70460f905
Mario Medina <mariomedinam@gmail.com>
2024-11-25 11:35:54 -0500

    Add ArrowRightIcon component for improved navigation

---
+++ react_apache_ubuntu/src/icons/ArrowRight.js
@@ -0,0 +1,21 @@
+const ArrowRightIcon = ({ className, width = 24, height = 24}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height={height}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={`icon icon-tabler icons-tabler-outline icon-tabler-arrow-right ${className}`}
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M5 12l14 0" />
+    <path d="M13 18l6 -6" />
+    <path d="M13 6l6 6" />
+  </svg>
+);
+
+export default ArrowRightIcon;
