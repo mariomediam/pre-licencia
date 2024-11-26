c816fef39ad66cafab5b4323fb87219a519d567f
Mario Medina <mariomedinam@gmail.com>
2024-11-26 08:01:15 -0500

    Refactor HeaderIdicators to use selectedYear and navigate on year change

--- react_apache_ubuntu/src/views/managementIndicators/HeaderIdicators.jsx
+++ react_apache_ubuntu/src/views/managementIndicators/HeaderIdicators.jsx
@@ -1,14 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import SideBarIcon from "../../icons/SideBarIcon";
 
+export const HeaderIdicators = ({ anios = [], setAnioSelected }) => {
+  const navigate = useNavigate();
+  const { selectedYear } = useSelector((state) => state.indicators);
 
-export const HeaderIdicators = ({anios = [], setAnioSelected}) => {
-  const { currentYear } = useSelector((state) => state.indicators);
-
-    const onChangeSelectYear = (e) => {
-        setAnioSelected( parseInt(e.target.value) );
-    }
-
+  const onChangeSelectYear = (e) => {
+    setAnioSelected(parseInt(e.target.value));
+    navigate(`/indicadores/${e.target.value}`);
+  };
 
   return (
     <>
@@ -19,15 +20,23 @@ export const HeaderIdicators = ({anios = [], setAnioSelected}) => {
           </div>
           <div className="m-0 p-0">
             <p className="m-0 p-0 fs-5 fw-bold">Indicadores de gestión</p>
-            <p className="p-0" style={{ marginTop: "-5px", marginBottom: "0px" }}>
+            <p
+              className="p-0"
+              style={{ marginTop: "-5px", marginBottom: "0px" }}
+            >
               Transportes
             </p>
           </div>
         </div>
 
         <div className="d-flex align-items-center gap-2 ">
-          <span className="m-0 p-0">Año: </span>
-          <select className="form-select " aria-label="Año a consultar" onChange={onChangeSelectYear} value={currentYear}>
+          <span className="m-0 p-0">{selectedYear} Año: </span>
+          <select
+            className="form-select "
+            aria-label="Año a consultar"
+            onChange={onChangeSelectYear}
+            value={selectedYear}
+          >
             {anios.map((anio) => (
               <option key={anio} value={anio}>
                 {anio}
