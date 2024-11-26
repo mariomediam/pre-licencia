c3af6e4d0f26b4a89b35194ae861a264b34cd3e5
Mario Medina <mariomedinam@gmail.com>
2024-11-26 08:02:03 -0500

    Implement navigation in ViewMore component to redirect based on provided URL

--- react_apache_ubuntu/src/components/managementIndicators/ViewMore.jsx
+++ react_apache_ubuntu/src/components/managementIndicators/ViewMore.jsx
@@ -1,9 +1,20 @@
 
+import { useNavigate } from 'react-router-dom'
 import ArrowRightIcon from '../../icons/ArrowRight'
 
-export const ViewMore = () => {
+export const ViewMore = ({ url }) => {
+
+  const navigate = useNavigate();
+
+  const onClickGoTo = () => {
+    navigate(url);
+  }
+  
+
+
+
   return (
-    <div className="cursor-pointer negrita-container" role="button" >
+    <div className="cursor-pointer negrita-container" role="button" onClick={onClickGoTo}>
         <small style={{ fontSize: "0.7rem" }} className="negrita-text">
           Ver más
         </small>
