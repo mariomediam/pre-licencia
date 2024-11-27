12c6e59df05758d3fb4fb765c158b7eaf7ae961d
Mario Medina <mariomedinam@gmail.com>
2024-11-27 15:00:38 -0500

    Refactor TransportationTicketsAnual component to clean up unused code and improve readability

--- react_apache_ubuntu/src/components/managementIndicators/transportationTickets/TransportationTicketsAnual.jsx
+++ react_apache_ubuntu/src/components/managementIndicators/transportationTickets/TransportationTicketsAnual.jsx
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom";
 import { InfraccionesTransporte } from "../../../services/indicatorsService";
 import MyChart from "../../helpers/MyChart";
 
-
 export const TransportationTicketsAnual = () => {
   const [optionChart, setOptionsChart] = useState({});
   const [formatData, setFormatData] = useState([]);
@@ -38,11 +37,9 @@ export const TransportationTicketsAnual = () => {
   useEffect(() => {
     const getVehiculosVigentes = async () => {
       try {
-        const infracciones = await InfraccionesTransporte();        
+        const infracciones = await InfraccionesTransporte();
         setFormatData(infracciones);
-
-        const listAnios = infracciones.map(({ anio }) => anio).reverse(); // Orden descendente
-        console.log("listAnios", listAnios)
+        const listAnios = infracciones.map(({ anio }) => anio).reverse(); // Orden descendente        
         const series = [
           {
             name: "Infracciones",
@@ -55,41 +52,8 @@ export const TransportationTicketsAnual = () => {
               focus: "series",
             },
             data: infracciones.map(({ q_total }) => q_total).reverse(),
-          }
-        ]
-
-        console.log("series", series)
-        // const objAnios = Object.groupBy(vehiculos, ({ anio }) => anio);
-        // const objTipos = Object.groupBy(vehiculos, ({ grouped }) => grouped);
-
-        // const listTipos = Object.keys(objTipos);
-        // const listAnios = Object.keys(objAnios).reverse(); // Orden descendente
-
-        // const series = listTipos.map((keyTipo) => {
-        //   const objAniosporTipo = Object.groupBy(
-        //     objTipos[keyTipo],
-        //     ({ anio }) => anio
-        //   );
-        //   return {
-        //     name: keyTipo,
-        //     type: "bar",
-        //     stack: "total",
-        //     label: {
-        //       show: true,
-        //     },
-        //     emphasis: {
-        //       focus: "series",
-        //     },
-        //     data: listAnios.map((anio) =>
-        //       objAniosporTipo[anio]
-        //         ? objAniosporTipo[anio].reduce(
-        //             (acc, { q_total }) => acc + q_total,
-        //             0
-        //           )
-        //         : 0
-        //     ),
-        //   };
-        // });
+          },
+        ];
 
         setOptionsChart({
           ...defaultOption,
@@ -106,41 +70,6 @@ export const TransportationTicketsAnual = () => {
     getVehiculosVigentes();
   }, [anioSelected, defaultOption]);
 
-  // useEffect(() => {
-  //   if (!optionChart.series || optionChart.series.length === 0) return;
-
-  //   const seriesData = optionChart.series.map((serie) => ({
-  //     ...serie,
-  //     data: serie.data.reverse(),
-  //   }));
-
-  //   const tipos = seriesData.map((serie) => serie.name);
-  //   tipos.push("TOTAL");
-  //   console.log("seriesData", seriesData);
-
-  //   seriesData.push({
-  //     name: "TOTAL",
-  //     type: "line",
-  //     stack: "total",
-  //     label: {
-  //       show: true,
-  //     },
-  //     emphasis: {
-  //       focus: "series",
-  //     },
-  //     data: seriesData.reduce(
-  //       (acc, { data }) => data.map((value, index) => acc[index] + value),
-  //       Array(seriesData[0].data.length).fill(0)
-  //     ),
-  //   });
-
-  //   setFormatData({
-  //     tipos: tipos,
-  //     anios: [...optionChart.yAxis.data].reverse(),
-  //     series: seriesData,
-  //   });
-  // }, [optionChart]);
-
   return (
     <div className="d-flex flex-column flex-grow-1 justify-content-between">
       <div className="d-flex flex-column align-items-center gap-2">
@@ -151,30 +80,6 @@ export const TransportationTicketsAnual = () => {
             heightChart="600px"
           />
           <div className="d-flex justify-content-center">
-            {/* <table className="table">
-              <thead>
-                <tr>
-                  <th className="px-3"></th>
-                  {formatData?.tipos?.map((tipo) => (
-                    <th key={tipo} className="px-3">
-                      {tipo}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {formatData?.anios?.map((anio, index) => (
-                  <tr key={anio} className="py-0 my-0">
-                    <td className="py-0 text-end">{anio}</td>
-                    {formatData?.series?.map((serie) => (
-                      <td key={serie.name} className="py-0 text-end">
-                        {serie.data[index]}
-                      </td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table> */}
             <table className="table">
               <thead>
                 <tr>
@@ -183,19 +88,17 @@ export const TransportationTicketsAnual = () => {
                 </tr>
               </thead>
               <tbody>
-                {formatData?.map(({anio, q_total}, index) => (
+                {formatData?.map(({ anio, q_total }, index) => (
                   <tr key={anio} className="py-0 my-0">
                     <td className="py-0 text-end">{anio}</td>
-                    <td  className="py-0 text-end">
-                      {q_total}
-                    </td>
+                    <td className="py-0 text-end">{q_total}</td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
         </div>
-      </div>      
+      </div>
     </div>
   );
-}
+};
