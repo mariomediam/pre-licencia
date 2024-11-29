c5bf1e2fcdccf76d34c8e2eb730a335e80a04230
Mario Medina <mariomedinam@gmail.com>
2024-11-29 13:24:41 -0500

    Enhance layout of CardItemIndicator by centering content

--- react_apache_ubuntu/src/components/managementIndicators/CardItemIndicator.jsx
+++ react_apache_ubuntu/src/components/managementIndicators/CardItemIndicator.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const CardItemIndicator = ({ indicator }) => {
   const { component } = indicator;
   return (
-    <div className="border rounded shadow-sm p-3 card-indicator d-flex flex-column">            
+    <div className="border rounded shadow-sm p-3 card-indicator d-flex flex-column justify-content-center align-items-center">            
       {component}
     </div>
   );
