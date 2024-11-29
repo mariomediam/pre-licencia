03e2e7cfc4f33c622046dc317c12aee2a1cc1e73
Mario Medina <mariomedinam@gmail.com>
2024-11-29 10:20:42 -0500

    Add selectedType state and setSelectedType reducer to indicatorsSlice

--- react_apache_ubuntu/src/store/slices/indicators/indicatorsSlice.js
+++ react_apache_ubuntu/src/store/slices/indicators/indicatorsSlice.js
@@ -5,6 +5,7 @@ export const indicatorsSlice = createSlice({
   initialState: {
     indicators: {},
     selectedYear: new Date().getFullYear(),    
+    selectedType: "01",
   },
   reducers: {
     initialIndicators: (state, { payload }) => {
@@ -16,8 +17,11 @@ export const indicatorsSlice = createSlice({
     setSelectedYear: (state, { payload }) => {      
       state.selectedYear = payload;
     },
+    setSelectedType: (state, { payload }) => {
+      state.selectedType = payload;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { initialIndicators, setIndicators, setSelectedYear } = indicatorsSlice.actions;
+export const { initialIndicators, setIndicators, setSelectedYear, setSelectedType } = indicatorsSlice.actions;
