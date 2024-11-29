864a64e6560f25cd79bbf4a49e7709861af3b145
Mario Medina <mariomedinam@gmail.com>
2024-11-29 10:20:57 -0500

    Add setSelectedTypeAndYearThunk to dispatch selected type and year

--- react_apache_ubuntu/src/store/slices/indicators/thunks.js
+++ react_apache_ubuntu/src/store/slices/indicators/thunks.js
@@ -1,6 +1,10 @@
-import { setSelectedYear } from "./indicatorsSlice";
+import { setSelectedType, setSelectedYear } from "./indicatorsSlice";
 
 export const setSelectedYearThunk = (year) => (dispatch) => {    
     dispatch(setSelectedYear(year));
 };
 
+export const setSelectedTypeAndYearThunk = (type, year) => (dispatch) => {    
+    dispatch(setSelectedYear(year));
+    dispatch(setSelectedType(type));
+}
