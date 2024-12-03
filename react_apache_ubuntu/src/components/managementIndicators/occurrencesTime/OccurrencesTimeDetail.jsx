1ad4e7f69756318ab2d70f40e62a780d588d4af3
Mario Medina <mariomedinam@gmail.com>
2024-12-03 14:20:08 -0500

    Refactor OccurrencesTimeDetail component to enhance chart data handling and improve rendering of monthly occurrences

--- react_apache_ubuntu/src/components/managementIndicators/occurrencesTime/OccurrencesTimeDetail.jsx
+++ react_apache_ubuntu/src/components/managementIndicators/occurrencesTime/OccurrencesTimeDetail.jsx
@@ -7,12 +7,12 @@ import MyChart from "../../helpers/MyChart";
 import XIcon from "../../../icons/XIcon";
 import { obtenerNombreMes } from "../../../utils/varios";
 
-
 export const OccurrencesTimeDetail = () => {
   const navigate = useNavigate();
   const { anio: urlYear } = useParams();
 
   const [dataOccurrence, setDataOccurrence] = useState([]);
+  const [dataOccurrenceFormat, setDataOccurrenceFormat] = useState([]);
   const [totalOccurrences, setTotalOccurrences] = useState(0);
   const [optionChart, setOptionsChart] = useState({});
   const [colors, setColors] = useState([]);
@@ -20,35 +20,26 @@ export const OccurrencesTimeDetail = () => {
   const dafaultOption = useMemo(
     () => ({
       tooltip: {
-        trigger: 'axis',
+        trigger: "axis",
         axisPointer: {
-          // Use axis to trigger tooltip
-          type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
-        }
+          type: "shadow",
+        },
       },
       legend: {
         show: false,
       },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-      },
       xAxis: {
-        type: 'value'
+        type: "category",
+        data: [], // Se asignará en el useEffect
       },
       yAxis: {
-        type: 'category',
-        // data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        type: "value",
       },
-      series: [      
-      ]
+      series: [],
     }),
     []
   );
 
- 
   useEffect(() => {
     const getOccurrences = async () => {
       try {
@@ -62,38 +53,48 @@ export const OccurrencesTimeDetail = () => {
         const objMes = Object.groupBy(dataOccurrence, ({ mes }) => mes);
         const listMes = Object.keys(objMes);
 
-        const objTipoApoyo = Object.groupBy(dataOccurrence, ({ tipo_de_apoyo }) => tipo_de_apoyo);
+        const objTipoApoyo = Object.groupBy(
+          dataOccurrence,
+          ({ tipo_de_apoyo }) => tipo_de_apoyo
+        );
         const listTipoApoyo = Object.keys(objTipoApoyo);
-
 
         console.log("listMes", listMes);
         console.log("listTipoApoyo", listTipoApoyo);
 
         const series = listTipoApoyo.map((tipoApoyo) => {
           const data = listMes.map((mes) => {
-            const item = dataOccurrence.find((item) => item.mes === parseInt(mes) && item.tipo_de_apoyo === tipoApoyo);
+            const item = dataOccurrence.find(
+              (item) =>
+                item.mes === parseInt(mes) && item.tipo_de_apoyo === tipoApoyo
+            );
             return item ? item.q_total : 0;
           });
           return {
-        
-          name: tipoApoyo,
-          type: 'bar',
-          stack: 'total',
-          label: {
-            show: true
-          },
-          emphasis: {
-            focus: 'series'
-          },
-          data
-        
+            name: tipoApoyo,
+            type: "bar",
+            stack: "total",
+            label: {
+              show: true,
+            },
+            emphasis: {
+              focus: "series",
+            },
+            data,
           };
         });
 
-        const sumOccurrences = dataOccurrence.reduce((acc, item) => acc + item.value, 0);
+        const sumOccurrences = dataOccurrence.reduce(
+          (acc, item) => acc + item.value,
+          0
+        );
         setTotalOccurrences(sumOccurrences);
-        
-        setDataOccurrence({legendData: listTipoApoyo, xAxisData: listMes.map((mes) => obtenerNombreMes(mes)), seriesData: series});
+
+        setDataOccurrence({
+          tipoApoyo: listTipoApoyo,
+          meses: listMes.map((mes) => obtenerNombreMes(mes)),
+          seriesData: series,
+        });
       } catch (error) {
         console.error(error);
       }
@@ -104,14 +105,90 @@ export const OccurrencesTimeDetail = () => {
   useEffect(() => {
     setOptionsChart({
       ...dafaultOption,
-      yAxis: { ...dafaultOption.yAxis, data: dataOccurrence.xAxisData },
+      xAxis: {
+        ...dafaultOption.xAxis,
+        data: dataOccurrence.meses,
+        type: "category",
+      },
+      yAxis: {
+        ...dafaultOption.yAxis,
+        type: "value",
+      },
       series: dataOccurrence.seriesData,
       legend: {
         ...dafaultOption.legend,
-        data: dataOccurrence.legendData
+        data: dataOccurrence.tipoApoyo,
       },
     });
   }, [dataOccurrence, dafaultOption]);
+
+  useEffect(() => {
+    // crear un nuevo array con los meses y los valores de cada mes de la siguiente manera
+    // [
+    // {
+    //   "MES": "Enero",
+    //  "value": 30
+    //   data: [
+    //     {
+    //       "tipo_de_apoyo": "Apoyo 1",
+    //       "value": 10
+    //     },
+    //     {
+    //       "tipo_de_apoyo": "Apoyo 2",
+    //       "value": 20
+    //     }
+    // },
+    // {
+    //   "MES": "Febrero",
+    // value: 30
+    //   data: [
+    //     {
+    //       "tipo_de_apoyo": "Apoyo 1",
+    //       "value": 10
+    //     },
+    //     {
+    //       "tipo_de_apoyo": "Apoyo 2",
+    //       "value": 20
+    //     }
+    // }
+
+    // const lisMeses = dataOccurrence.xAxisData;
+    console.log("first dataOccurrence", dataOccurrence);
+
+    // const seriesData = dataOccurrence.seriesData;
+
+    // const data = dataOccurrence.meses.map((mes, index) => {
+    //   const obj = {
+    //     mes,
+    //     data: seriesData.map((item) => ({
+    //       tipo_de_apoyo: item.name,
+    //       value: item.data[index],
+    //     })),
+    //   };
+    //   return obj;
+    // });
+
+    const data = dataOccurrence?.meses?.map((mes, index) => {
+      const obj = {
+        mes,
+        value: dataOccurrence.seriesData.reduce(
+          (acc, item) => acc + item.data[index],
+          0
+        ),
+        data: dataOccurrence.seriesData.map((item) => ({
+          tipo_de_apoyo: item.name,
+          value: item.data[index],
+        })),
+      };
+      return obj;
+    });
+
+    console.log(data);
+
+    setDataOccurrenceFormat(data);
+
+    // ]
+  }, [dataOccurrence]);
 
   const onClickClose = () => {
     navigate(-1);
@@ -144,18 +221,68 @@ export const OccurrencesTimeDetail = () => {
         <div className="d-flex flex-column flex-grow-1 align-items-center justify-content-between pt-0">
           <div className="d-flex flex-column align-items-center gap-2">
             {/* <h3 className="m-0 pt-2 pb-0"> {totalOccurrences} ocurrencias</h3> */}
-            <div className="d-flex flex-column gap-0 flex-wrap justify-content-center align-items-center">
-              <MyChart
-                option={optionChart}
-                widthChart="550px"
-                heightChart="550px"
-                onColorsChange={setColors}
-              />{" "}
-              <div
-                className="d-flex justify-content-center pt-4 px-2"
-               
-              >
-                {/* aqui van los valores de la tabla */}
+            <div className="d-flex flex-column gap-0 flex-wrap justify-content-center align-items-center border">
+              <div className="mychart">
+                <MyChart
+                  option={optionChart}
+                  widthChart="100%"
+                  heightChart="550px"
+                  onColorsChange={setColors}
+                />
+              </div>
+            </div>
+            <div className="container">
+              <div className="row">
+                <div className="d-flex justify-content-center ">
+                  <Accordion
+                    defaultActiveKey="0"
+                    flush
+                    className="col-sm-12 border"
+                  >
+                    {dataOccurrenceFormat?.map((item, index) => (
+                      <Accordion.Item key={index} eventKey={index}>
+                        <Accordion.Header>
+                          <div className="d-flex justify-content-between w-100">
+                            <div className="">
+                              <p className="m-0 p-0">{item.mes}</p>
+                            </div>
+                            <div className="pe-2">
+                              <p className="m-0 p-0">{item.value}</p>
+                            </div>
+                          </div>
+                        </Accordion.Header>
+                        <Accordion.Body>
+                          <div className="d-flex flex-column gap-2 ps-3 pe-4">
+                            {item.data.map((itemData, indexData) => (
+                              <div
+                                key={indexData}
+                                className="d-flex justify-content-between  gap-5"
+                              >
+                                <div className="d-flex align-items-start">
+                                  <span
+                                    className="circle-icon me-1 mt-1"
+                                    style={{
+                                      backgroundColor: colors[indexData],
+                                      width: "16px",
+                                      height: "16px",
+                                      flexShrink: 0,
+                                    }}
+                                  ></span>
+                                  <p className="m-0 p-0">
+                                    {itemData.tipo_de_apoyo}
+                                  </p>
+                                </div>
+                                <div>
+                                  <p className="m-0 p-0">{itemData.value}</p>
+                                </div>
+                              </div>
+                            ))}
+                          </div>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                    ))}
+                  </Accordion>
+                </div>
               </div>
             </div>
           </div>
@@ -163,4 +290,4 @@ export const OccurrencesTimeDetail = () => {
       </main>
     </div>
   );
-}
+};
