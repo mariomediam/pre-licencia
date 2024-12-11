628c3903adfc755d86831898b665caee8fb71d4c
Mario Medina <mariomedinam@gmail.com>
2024-12-11 13:23:28 -0500

    Add MontosPapeleta and ComparacionMontosPapeleta functions to indicatorsService for fetching data

--- react_apache_ubuntu/src/services/indicatorsService.js
+++ react_apache_ubuntu/src/services/indicatorsService.js
@@ -153,6 +153,55 @@ const OcurrenciasxAnio = async ( params ) => {
   }
 }
 
+const MontosPapeleta = async ( params ) => {
+  
+    const { anio, tipo } = params;
+  
+    let credenciales = {};
+  
+    try {
+      const headers = {
+        "Content-Type": "application/json",
+      };
+      let {
+        data: { content },
+      } = await axios.get(`${URL}/montos-papeleta?anio=${anio}&tipo=${tipo}`, credenciales, {
+        headers,
+      });
+  
+      return content;
+    }
+    catch (error) {
+      throw error;
+    }
+  }
+
+  // {{url_api}}/api/transporte/comparacion-montos-papeleta?dia=21&mes=11&anio01=2023&anio02=2024
+
+  const ComparacionMontosPapeleta = async ( params ) => {
+    
+      const { dia, mes, anio01, anio02 } = params;
+    
+      let credenciales = {};
+    
+      try {
+        const headers = {
+          "Content-Type": "application/json",
+        };
+        let {
+          data: { content },
+        } = await axios.get(`${URL}/comparacion-montos-papeleta?dia=${dia}&mes=${mes}&anio01=${anio01}&anio02=${anio02}`, credenciales, {
+          headers,
+        });
+    
+        return content;
+      }
+      catch (error) {
+        throw error;
+      }
+    }
+
+
 
 export {
   TranspVigente,
@@ -162,4 +211,6 @@ export {
   ComparacionInfraccionesTransporte,
   AntiguedadVehiculos,
   OcurrenciasxAnio,
+  MontosPapeleta,
+  ComparacionMontosPapeleta,
 };
