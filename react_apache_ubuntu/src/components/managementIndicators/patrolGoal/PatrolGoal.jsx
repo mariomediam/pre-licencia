e42a40db559f7edaab1ceb5e4a86b50de9133013
Mario Medina <mariomedinam@gmail.com>
2024-12-10 22:26:01 -0500

    Refactor PatrolGoal component to utilize useMemo for data handling, add icons for performance indicators, and improve chart configuration with mark points

--- react_apache_ubuntu/src/components/managementIndicators/patrolGoal/PatrolGoal.jsx
+++ react_apache_ubuntu/src/components/managementIndicators/patrolGoal/PatrolGoal.jsx
@@ -3,34 +3,81 @@ import { useMemo } from "react";
 import MyChart from "../../helpers/MyChart";
 import { ViewMore } from "../ViewMore";
 
-export const PatrolGoal = ({ anioSelected, title = "" }) => {
-  const rawData = [
-    [70, 72, 96, 94],
-    [30, 28, 4, 6],
-  ];
-  const totalData = [100, 100, 100, 100];
+import upIcon from "../../../assets/images/indicators/thumb-up.png";
+import downIcon from "../../../assets/images/indicators/thumb-down.png";
 
-  const series = ["Cumplido", "Faltante"].map((name, sid) => {
-    return {
-      name,
-      type: "bar",
-      stack: "total",
-      barWidth: "60%",
-      label: {
-        show: true,
-        formatter: (params) => Math.round(params.value * 1000) / 10 + "%",
-        fontSize: 10,
-      },
-      data: rawData[sid].map((d, did) =>
-        totalData[did] <= 0 ? 0 : d / totalData[did]
-      ),
-    };
+export const PatrolGoal = ({ anioSelected, title = "" }) => {
+  const urlUpIcon = `image://${upIcon}`;
+  const urlDownIcon = `image://${downIcon}`;
+
+  const rawData = useMemo(
+    () => [
+      [70, 72, 96, 94],
+      [30, 28, 4, 6],
+    ],
+    []
+  );
+  const totalData = useMemo(() => [100, 100, 100, 100], []);
+
+  const cumplimientoPorcentajes = rawData[0].map((cumplido, index) => {
+    const total = totalData[index];
+    return total <= 0 ? 0 : cumplido / total;
   });
+
+  const series = useMemo(
+    () => [
+      {
+        name: "CUMPLIDO",
+        type: "bar",
+        stack: "total",
+        barWidth: "60%",
+        label: {
+          show: true,
+          formatter: (params) => Math.round(params.value * 1000) / 10 + "%",
+        },
+        data: rawData[0].map((d, did) =>
+          totalData[did] <= 0 ? 0 : d / totalData[did]
+        ),
+
+        markPoint: {
+          data: cumplimientoPorcentajes.map((porcentaje, index) => ({
+            xAxis: index,
+            y: porcentaje,
+            symbol: porcentaje >= 0.75 ? urlUpIcon : urlDownIcon,
+            symbolSize: 20,
+            symbolOffset: [0, 40],
+          })),
+          label: {
+            show: false,
+          },
+        },
+      },
+      {
+        name: "FALTANTE",
+        type: "bar",
+        stack: "total",
+        barWidth: "60%",
+        label: {
+          show: true,
+          formatter: (params) => Math.round(params.value * 1000) / 10 + "%",
+        },
+        data: rawData[1].map((d, did) =>
+          totalData[did] <= 0 ? 0 : d / totalData[did]
+        ),
+      },
+    ],
+    [rawData, totalData, urlUpIcon, urlDownIcon, cumplimientoPorcentajes]
+  );
 
   const dafaultOption = useMemo(
     () => ({
       legend: {
         selectedMode: false,
+      },
+      grid: {
+        // top: "3%",
+        bottom: "5%",
+        containLabel: true,
       },
       yAxis: {
         type: "value",
@@ -59,6 +106,9 @@ export const PatrolGoal = ({ anioSelected, title = "" }) => {
             heightChart="200px"
           />{" "}
         </div>
+        <small style={{fontSize: "0.7rem"}}>
+          El patrullaje cumple la meta si el porcentaje es mayor o igual al 75%.
+        </small>
       </div>
       <ViewMore url={`/indicadores/patullaje-meta/${anioSelected}`} />
     </div>
