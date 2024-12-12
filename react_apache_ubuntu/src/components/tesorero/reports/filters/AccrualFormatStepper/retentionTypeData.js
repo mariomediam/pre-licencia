87c34de73ea2d13454668b8a65a0104a0f422efa
Mario Medina <mariomedinam@gmail.com>
2024-12-12 15:22:54 -0500

    Add retention type data for AccrualFormatStepper

---
+++ react_apache_ubuntu/src/components/tesorero/reports/filters/AccrualFormatStepper/retentionTypeData.js
@@ -0,0 +1,27 @@
+export const getRetentionType = () => [
+    {
+      code: "01",
+      description: "DETRACCIÓN",
+      value: 0.00,
+    },
+    {
+        code: "02",
+        description: "PENALIDAD",
+        value: 0.00,
+    },
+    {
+        code: "03",
+        description: "RETENCIÓN DEL 10%",
+        value: 0.00,
+    },
+    {
+        code: "04",
+        description: "RENTA DE 4TA",
+        value: 0.00,
+    },
+    {
+        code: "05",
+        description: "EMBARGO A FAVOR DE SUNAT",
+        value: 0.00,
+    },
+  ];
