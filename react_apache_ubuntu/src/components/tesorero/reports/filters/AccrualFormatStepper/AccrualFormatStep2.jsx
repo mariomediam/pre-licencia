fe4436ece7a896be62c850073529f13128ad7633
Mario Medina <mariomedinam@gmail.com>
2024-12-13 11:26:13 -0500

    Refactor AccrualFormatStep2 component to accept retentions and error handling props

--- react_apache_ubuntu/src/components/tesorero/reports/filters/AccrualFormatStepper/AccrualFormatStep2.jsx
+++ react_apache_ubuntu/src/components/tesorero/reports/filters/AccrualFormatStepper/AccrualFormatStep2.jsx
@@ -5,20 +5,24 @@ import { AccuralRetentionAdd } from "./AccuralRetentionAdd";
 import { useSelector } from "react-redux";
 import { formatNumber } from "../../../../../utils/varios";
 
-export const AccrualFormatStep2 = () => {
+export const AccrualFormatStep2 = ({retentions, setRetentions, expedErrors, setExpedErrors}) => {
   const { currentSecuencia } = useSelector((state) => state.siaf);
   const { MONTO_NACIONAL } = currentSecuencia;
   
 
-  const [retentions, setRetentions] = useState([]);
   const [sumRetentions, setSumRetentions] = useState(0)
 
  
 
-  useState(() => {
+  // useState(() => {
+  //   const retentionType = getRetentionType();
+  //   setRetentions(retentionType);
+  // }, []);
+
+  useEffect(() => {
     const retentionType = getRetentionType();
     setRetentions(retentionType);
-  }, []);
+  }, [setRetentions]);
 
   useEffect(() => {
     const totalRetentions = retentions.reduce(
@@ -31,12 +35,14 @@ export const AccrualFormatStep2 = () => {
   return (
     <div className="mx-5">
       <small className="fw-bold">Retenciones</small>
-      <div class="container px-0">
+      <div className="container px-0">
         {retentions.map((retention) => (
           <AccruralRetentionItem
             key={retention.code}
             retention={retention}
             setRetentions={setRetentions}
+            expedErrors={expedErrors}
+            setExpedErrors={setExpedErrors}
           />
         ))}
       </div>
