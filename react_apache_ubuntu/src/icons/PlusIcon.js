1216ca5f268ca7ed505748246588e79c4d865ff9
Mario Medina <mariomedinam@gmail.com>
2024-12-13 11:24:50 -0500

    Fix class attribute to className in PlusIcon component for proper JSX syntax

--- react_apache_ubuntu/src/icons/PlusIcon.js
+++ react_apache_ubuntu/src/icons/PlusIcon.js
@@ -9,7 +9,7 @@ const PlusIcon = ({ className, width = 24, height = 24 }) => (
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
-    class={`icon icon-tabler icons-tabler-outline icon-tabler-plus ${className}`}
+    className={`icon icon-tabler icons-tabler-outline icon-tabler-plus ${className}`}
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M12 5l0 14" />
