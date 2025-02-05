3e38d44ae9a09d29672583915c3f7b102ab23650
Mario Medina <mariomedinam@gmail.com>
2025-02-05 12:19:57 -0500

    Add carta orden selection and download functionality in TrustFormatView

--- react_apache_ubuntu/src/views/tesorero/reportes/TrustFormatView.jsx
+++ react_apache_ubuntu/src/views/tesorero/reportes/TrustFormatView.jsx
@@ -4,15 +4,29 @@ import { Breadcrumb } from "react-bootstrap";
 import Header from "../../../components/Header";
 import FilesIcon from "../../../icons/FilesIcon";
 import { TrustFormatModalComponent } from "../../../components/tesorero/reports/filters/TrustFormat/TrustFormatModalComponent";
-// import { AccrualFormatStepperView } from "./AccrualFormatStepperView";
+import { TrustFormatItemComponent } from "../../../components/tesorero/reports/filters/TrustFormat/TrustFormatItemComponent";
+import PlusIcon from "../../../icons/PlusIcon";
+import DownloadIcon from "../../../icons/DownloadIcon";
 
 export const TrustFormatView = () => {
-
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  
+  const [cartasSelected, setCartasSelected] = useState([]);
+
+  const addCartasSelected = (cartas) => {
+    const cartasSelectedKeys = cartasSelected.map((carta) => carta.key);
+    const cartasToAdd = cartas.filter(
+      (carta) => !cartasSelectedKeys.includes(carta.key)
+    );
+    setCartasSelected([...cartasSelected, ...cartasToAdd]);
+  };
+
+  const removeCartaSelected = (key) => {    
+    const cartas = cartasSelected.filter((carta) => carta.key !== key);
+    setCartasSelected(cartas);
+  };
 
   return (
     <>
@@ -29,11 +43,29 @@ export const TrustFormatView = () => {
       </div>
       <div className="d-flex justify-content-center pt-3 pb-3">
         <div style={{ maxWidth: "576px" }}>
-          {/* <AccrualFormatStepperView /> */}
+          <div className="p-2">
+            {cartasSelected.map((item, index) => (
+              <TrustFormatItemComponent
+                key={item.key}
+                cartaOrden={item}
+                removeCartaSelected={removeCartaSelected}
+              />
+            ))}
+            <div className="d-grid ">
+              <button
+                className="btn btn-primary"
+                title="Descargar documento"
+                // onClick={handleShow}
+                hidden={cartasSelected.length === 0}
+              >
+                Descargar Documento <DownloadIcon className="ms-2" />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div style={{ position: "relative" }}>
+      {/* <div style={{ position: "relative" }}>
         <div style={{ position: "absolute", right: "300px", width: "70px" }}>
           <div style={{ position: "fixed", bottom: "45px" }}>
             <button
@@ -41,15 +73,29 @@ export const TrustFormatView = () => {
               style={{ width: "70px", height: "70px" }}
               title="Agregar carta orden"
               onClick={handleShow}
-              // disabled={periodosDisponibles.length === 0}
             >
               <i className="fas fa-plus"></i>
             </button>
           </div>
         </div>
+      </div> */}
+
+      <div className="agregar-btn">
+        <button
+          className="btn btn-primary rounded-circle"
+          style={{ width: "70px", height: "70px" }}
+          title="Agregar carta orden"
+          onClick={handleShow}
+        >
+          <PlusIcon />
+        </button>
       </div>
 
-      <TrustFormatModalComponent show={show} handleClose={handleClose} />
+      <TrustFormatModalComponent
+        show={show}
+        handleClose={handleClose}
+        addCartasSelected={addCartasSelected}
+      />
     </>
   );
 };
