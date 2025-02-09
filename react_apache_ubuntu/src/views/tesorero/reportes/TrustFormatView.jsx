3aeca9012bd02f3142889e06bdd5f11d31187e5a
Mario Medina <mariomedinam@gmail.com>
2025-02-08 22:55:48 -0500

    Add loading state and download functionality for Carta Orden in TrustFormatView

--- react_apache_ubuntu/src/views/tesorero/reportes/TrustFormatView.jsx
+++ react_apache_ubuntu/src/views/tesorero/reportes/TrustFormatView.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
-import { Breadcrumb } from "react-bootstrap";
+import { Breadcrumb, Spinner } from "react-bootstrap";
 
 import Header from "../../../components/Header";
 import FilesIcon from "../../../icons/FilesIcon";
 import { TrustFormatModalComponent } from "../../../components/tesorero/reports/filters/TrustFormat/TrustFormatModalComponent";
 import { TrustFormatItemComponent } from "../../../components/tesorero/reports/filters/TrustFormat/TrustFormatItemComponent";
+import { downloadCartaOrdenFideicomiso } from "../../../services/siafService";
 import PlusIcon from "../../../icons/PlusIcon";
 import DownloadIcon from "../../../icons/DownloadIcon";
+import { AlertStartSearch } from "../../../components/tools/AlertStartSearch";
 
 export const TrustFormatView = () => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [cartasSelected, setCartasSelected] = useState([]);
 
@@ -23,9 +26,27 @@ export const TrustFormatView = () => {
     setCartasSelected([...cartasSelected, ...cartasToAdd]);
   };
 
-  const removeCartaSelected = (key) => {    
+  const removeCartaSelected = (key) => {
     const cartas = cartasSelected.filter((carta) => carta.key !== key);
     setCartasSelected(cartas);
+  };
+
+  const downloadDocumento = async () => {
+    try {
+      setIsLoading(true);
+      const file = await downloadCartaOrdenFideicomiso({
+        cartas: cartasSelected,
+      });
+      const url = window.URL.createObjectURL(file);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "CartaOrdenFideicomiso.xlsx";
+      a.click();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -41,8 +62,18 @@ export const TrustFormatView = () => {
         <FilesIcon className="me-1 thumbnail text-color-default mb-1" />
         <h3 className="text-color-default">Formato para fideicomiso</h3>
       </div>
+
       <div className="d-flex justify-content-center pt-3 pb-3">
         <div style={{ maxWidth: "576px" }}>
+
+          {cartasSelected.length === 0 && (
+            <div className="d-flex justify-content-center">
+              <div className="col-sm-6 col-md-9 ">
+              <AlertStartSearch parametros={{ message: "Por favor, agregue una o más cartas orden." }}/>
+              </div>
+            </div>
+          )}
+
           <div className="p-2">
             {cartasSelected.map((item, index) => (
               <TrustFormatItemComponent
@@ -55,10 +86,24 @@ export const TrustFormatView = () => {
               <button
                 className="btn btn-primary"
                 title="Descargar documento"
-                // onClick={handleShow}
+                onClick={downloadDocumento}
                 hidden={cartasSelected.length === 0}
               >
-                Descargar Documento <DownloadIcon className="ms-2" />
+                {isLoading ? (
+                  <>
+                    <Spinner
+                      animation="border"
+                      role="status"
+                      size="sm"
+                      className="me-2"
+                    />
+                    Generando documento
+                  </>
+                ) : (
+                  <>
+                    Descargar Documento <DownloadIcon className="ms-2" />
+                  </>
+                )}
               </button>
             </div>
           </div>
