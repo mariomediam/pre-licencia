baaae2b0d7852de4d4c8a532b807cc6c48beda57
Mario Medina <mariomedinam@gmail.com>
2025-05-19 14:33:23 -0500

    Implementar selección de año y mes en el componente SeguimientoProyecto, incluyendo búsqueda de proyectos por código o descripción. Se añaden estados para manejar la selección y se mejora la estructura del componente.

--- react_apache_ubuntu/src/views/territorial/proyectosInversion/SeguimientoProyecto.jsx
+++ react_apache_ubuntu/src/views/territorial/proyectosInversion/SeguimientoProyecto.jsx
@@ -1,34 +1,133 @@
-import { Breadcrumb } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import {
+  Breadcrumb,
+  Form,
+  InputGroup,
+  Button,
+  Row,
+  Col,
+} from "react-bootstrap";
+import { Mes } from "../../../utils";
+import { useParams } from "react-router-dom";
 
 import Header from "../../../components/Header";
 import ChartHistogramIcon from "../../../icons/ChartHistogramIcon";
-// import { GenerarBoletaComponent } from "../../../../components/rrhh";
+import SearchIcon from "../../../icons/SearchIcon";
+import { SeguimientoProyectoMes } from "../../../components/territorial/proyectosInversion/SeguimientoProyectoMes";
+
+const anioActual = () => {
+  const fecha = new Date();
+  return fecha.getFullYear();
+};
+
+const mesActual = () => {
+  const fecha = new Date();
+  return fecha.getMonth() + 1;
+};
+
+const currentYear = new Date().getFullYear();
+const anios = Array.from(
+  { length: currentYear - 2020 },
+  (_, i) => currentYear - i
+);
 
 export const SeguimientoProyecto = () => {
-    return (
-        <div>
-          <Header />
-          <div className="ps-3">
-            <Breadcrumb>
-              <Breadcrumb.Item active>Territorial</Breadcrumb.Item>
-              <Breadcrumb.Item active>Proyectos de inversión</Breadcrumb.Item>
-            </Breadcrumb>
+  
+
+  let { anio = anioActual(), mes = mesActual() } = useParams();
+
+  const [selectedAnio, setSelectedAnio] = useState(anio);
+  const [selectedMonth, setSelectedMonth] = useState(mes);
+
+  useEffect(() => {
+    const secEjec = process.env.SEC_EJEC;
+    console.log("Variable de entorno SEC_EJEC:", secEjec);
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <div className="ps-3">
+        <Breadcrumb>
+          <Breadcrumb.Item active>Territorial</Breadcrumb.Item>
+          <Breadcrumb.Item active>Proyectos de inversión</Breadcrumb.Item>
+        </Breadcrumb>
+      </div>
+      <hr className="p-0 m-0" />
+      <div className="container">
+        <div className="row justify-content-center">
+          <div
+            className="align-items-center p-2 col-sm-12 col-lg-8"
+            style={{ border: "0px solid black" }}
+          >
+            <h3 className="mt-0 text-center">
+              <ChartHistogramIcon className="me-2 mb-1" />
+              Seguimiento de Proyectos de Inversión
+            </h3>
           </div>
-            <hr className="p-0 m-0" />
-          <div className="container">
-            <div className="row justify-content-center">
-              <div
-                className="align-items-center p-2 col-sm-12 col-lg-8"
-                style={{ border: "0px solid black" }}
-              >
-                <h3 className="mt-0 text-center">
-                  <ChartHistogramIcon className="me-2 mb-1" />
-                  Seguimiento de Proyectos de Inversión
-                </h3>
-              </div>
+        </div>
+      </div>
+      <div className="container border">
+        <div className="row d-flex  justify-content-between my-2">
+          <div className="d-flex  justify-content-between  flex-wrap">
+            <Row className="col-12 col-lg-4 p-0 m-0 gap-3">
+              {/* Campo Año */}
+              <Col className="m-0 p-0">
+                <Form.Group controlId="anio">
+                  <Form.Label>Año</Form.Label>
+                  <Form.Select
+                    aria-label="Año del proyecto "
+                    className="mb-0"
+                    defaultValue={selectedAnio}
+                    onChange={(e) => setSelectedAnio(e.target.value)}
+                  >
+                    {anios.map((anio) => (
+                      <option key={anio} value={anio}>
+                        {anio}
+                      </option>
+                    ))}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+
+              {/* Campo Mes */}
+              <Col className="m-0 p-0">
+                <Form.Group controlId="mes">
+                  <Form.Label>Mes</Form.Label>
+                  <Mes
+                    selectedMonth={selectedMonth}
+                    setSelectedMonth={setSelectedMonth}
+                    className="mb-0"
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <div className="col-12 col-lg-5 p-0 m-0">
+              <Form.Label htmlFor="selectMes" className="">
+                Buscar proyecto
+              </Form.Label>
+              <InputGroup className="">
+                <Form.Control
+                  placeholder="Buscar por código o descripción..."
+                  aria-label="Buscar por código o descripción..."
+                  aria-describedby="Buscar por código o descripción..."
+                />
+                <Button variant="outline-secondary" id="button-addon2">
+                  <SearchIcon />
+                </Button>
+              </InputGroup>
             </div>
           </div>
-          {/* <GenerarBoletaComponent /> */}
+          <SeguimientoProyectoMes
+            selectedAnio={selectedAnio}
+            selectedMonth={selectedMonth}
+          />
         </div>
-      );
-}
+      </div>
+      <div>
+        <p>{selectedAnio}</p>
+        <p>{selectedMonth}</p>
+      </div>
+    </div>
+  );
+};
