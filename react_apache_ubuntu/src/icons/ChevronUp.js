ae362888ae206144379840e740e986852ce284f9
Mario Medina <mariomedinam@gmail.com>
2025-05-19 14:32:28 -0500

    Refactor componentes de íconos: actualizar atributos de SVG a camelCase y agregar propiedad style al componente ChevronUp. Añadir nuevo componente EditIcon.

--- react_apache_ubuntu/src/icons/ChevronUp.js
+++ react_apache_ubuntu/src/icons/ChevronUp.js
@@ -1,4 +1,4 @@
-const ChevronUp = ({ className, width=24, height=24 }) => (
+const ChevronUp = ({ className, width=24, height=24, style }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={width}
@@ -10,6 +10,7 @@ const ChevronUp = ({ className, width=24, height=24 }) => (
     strokeLinecap="round"
     strokeLinejoin="round"
     className={`icon icon-tabler icons-tabler-outline icon-tabler-chevron-up ${className}`}
+    style={style}
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M6 15l6 -6l6 6" />
