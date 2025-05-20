37c672e3daa7d0fd94e009d6401878265d6bca4f
Mario Medina <mariomedinam@gmail.com>
2025-05-20 09:55:09 -0500

    Agregar funcionalidad de filtrado en el componente SeguimientoProyectoMes, permitiendo buscar proyectos por código o nombre.

--- react_apache_ubuntu/src/components/territorial/proyectosInversion/SeguimientoProyectoMes.jsx
+++ react_apache_ubuntu/src/components/territorial/proyectosInversion/SeguimientoProyectoMes.jsx
@@ -2,8 +2,34 @@ import { useEffect, useState } from "react";
 import { obtenerProyectosProgramacionMensual } from "../../../services/siafService";
 import { SeguimentoProyectoMesItem } from "./SeguimentoProyectoMesItem";
 
-export const SeguimientoProyectoMes = ({ selectedAnio, selectedMonth }) => {
+export const SeguimientoProyectoMes = ({
+  selectedAnio,
+  selectedMonth,
+  searchTerm,
+}) => {
   const [proyectos, setProyectos] = useState([]);
+  const [filteredProyectos, setFilteredProyectos] = useState([]);
+
+  useEffect(() => {
+    const filterProyectos = (data) => {
+      if (searchTerm) {
+        if (!isNaN(searchTerm)) {
+          return data.filter((proyecto) =>
+            proyecto.c_proinv_codigo.includes(searchTerm)
+          );
+        } else {
+          return data.filter((proyecto) =>
+            proyecto.n_proinv_nombre
+              .toLowerCase()
+              .includes(searchTerm.toLowerCase())
+          );
+        }
+      }
+      return data;
+    };
+    const filteredData = filterProyectos(proyectos);
+    setFilteredProyectos(filteredData);
+  }, [searchTerm, proyectos]);
 
   useEffect(() => {
     const getProyectos = async () => {
@@ -12,16 +38,19 @@ export const SeguimientoProyectoMes = ({ selectedAnio, selectedMonth }) => {
         mes_eje: selectedMonth,
         sec_ejec: process.env.REACT_APP_SEC_EJEC,
       });
+
       setProyectos(data);
     };
     getProyectos();
   }, [selectedAnio, selectedMonth]);
+
   return (
     <div className="mt-3">
-      
-      
-      {proyectos.map((proyecto) => (
-        <SeguimentoProyectoMesItem key={proyecto.c_proinv} proyecto={proyecto} />
+      {filteredProyectos.map((proyecto) => (
+        <SeguimentoProyectoMesItem
+          key={proyecto.c_proinv}
+          proyecto={proyecto}
+        />
       ))}
     </div>
   );
