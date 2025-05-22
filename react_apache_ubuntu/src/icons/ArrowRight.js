029cc4e8357f8b441c6c6d96509503a0e7838732
Mario Medina <mariomedinam@gmail.com>
2025-05-22 08:13:27 -0500

    Agregar propiedad 'style' al componente ArrowRightIcon para permitir la personalización de estilos en el SVG.

--- react_apache_ubuntu/src/icons/ArrowRight.js
+++ react_apache_ubuntu/src/icons/ArrowRight.js
@@ -1,4 +1,4 @@
-const ArrowRightIcon = ({ className, width = 24, height = 24}) => (
+const ArrowRightIcon = ({ className, width = 24, height = 24, style }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={width}
@@ -10,6 +10,7 @@ const ArrowRightIcon = ({ className, width = 24, height = 24}) => (
     strokeLinecap="round"
     strokeLinejoin="round"
     className={`icon icon-tabler icons-tabler-outline icon-tabler-arrow-right ${className}`}
+    style={style}
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M5 12l14 0" />
