5a8339b9569026b8ba86064a3be5b3e4b409af70
Mario Medina <mariomedinam@gmail.com>
2025-07-03 14:13:34 -0500

    Agregar un spinner de carga en el componente AccrualFormatStep1

--- react_apache_ubuntu/src/components/tesorero/reports/filters/AccrualFormatStepper/AccrualFormatStep1.jsx
+++ react_apache_ubuntu/src/components/tesorero/reports/filters/AccrualFormatStepper/AccrualFormatStep1.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import { Spinner } from "react-bootstrap";
 import { addPropertyCurrentExpedThunk } from "../../../../../store/slices/siaf/thunks";
 import { obtenerExpedienteFase } from "../../../../../services/siafService";
 import { AccrualFormatExpedItem } from "./AccrualFormatExpedItem";
@@ -10,6 +10,7 @@ export const AccrualFormatStep1 = ({ expedErrors, setExpedErrors }) => {
   const dispatch = useDispatch();
   const { currentExped } = useSelector((state) => state.siaf);
   const { anioExped, numeroExped } = currentExped;
+  const [isLoading, setIsLoading] = useState(false);
 
   const [expedFases, setExpedFases] = useState([]);
 
@@ -26,14 +27,21 @@ export const AccrualFormatStep1 = ({ expedErrors, setExpedErrors }) => {
   useEffect(() => {
     const getExpediente = async () => {
       if (numeroExped.length === 10) {
-        const data = await obtenerExpedienteFase({
-          anio: anioExped,
-          expediente: numeroExped,
-          ciclo: "G",
-          fase: "D",
-        });
-        console.log("first data", data);
-        setExpedFases(data);
+        try {
+          setIsLoading(true);
+          console.log("loading");
+          const data = await obtenerExpedienteFase({
+            anio: anioExped,
+            expediente: numeroExped,
+            ciclo: "G",
+            fase: "D",
+          });          
+          setExpedFases(data);
+        } catch (error) {
+          console.log("error", error);
+        } finally {
+          setIsLoading(false);
+        }
       }
     };
     getExpediente();
@@ -44,6 +52,12 @@ export const AccrualFormatStep1 = ({ expedErrors, setExpedErrors }) => {
       <small>
         Expediente {numeroExped} {anioExped}
       </small>
+      {isLoading && (
+        <div className="d-flex mt-2 justify-content-center align-items-center">
+          <Spinner animation="border me-2" variant="primary" /> <small>Buscando devengados...</small>
+        </div>
+      )}
+
       {expedFases.map((item, index) => (
         <AccrualFormatExpedItem
           key={index}
@@ -54,6 +68,10 @@ export const AccrualFormatStep1 = ({ expedErrors, setExpedErrors }) => {
       {/* {
        <small className="text-danger"> {expedErrors.selectedSecuencia}</small>
       } */}
+
+     
+
+
 
       {expedErrors.selectedSecuencia && (
         <small className="text-danger d-flex align-items-center">
