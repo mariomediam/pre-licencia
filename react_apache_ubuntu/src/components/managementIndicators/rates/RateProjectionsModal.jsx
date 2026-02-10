00af6a5c7bd492c9bf4dce830f8dd53d40ac2991
Mario Medina <mariomedinam@gmail.com>
2026-02-10 10:42:35 -0500

    Refactorizar RateProjectionsModal para eliminar código comentado y optimizar la lógica de proyecciones. Se agrega la función formatMoney para mostrar el total proyectado, mejorando la presentación de datos en el modal.

--- react_apache_ubuntu/src/components/managementIndicators/rates/RateProjectionsModal.jsx
+++ react_apache_ubuntu/src/components/managementIndicators/rates/RateProjectionsModal.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
-import { Modal, Button, Spinner, Form, InputGroup } from "react-bootstrap";
+import { Modal, Button, Spinner } from "react-bootstrap";
 import TrendingUpIcon from "../../../icons/TrendingUpIcon";
 import { SelectProyeccionPorAnioYTasa, InsertarProyecciones } from "../../../services/indicatorsService";
-import { obtenerNombreMes } from "../../../utils/varios";
+import { obtenerNombreMes, formatMoney } from "../../../utils/varios";
 
 
 
@@ -51,7 +51,7 @@ export const RateProjectionsModal = ({
             for (let i = 1; i <= 12; i++) {
                 dataAllMonths.push({
                     M_Mes: i,
-                    Q_Proyecc_Monto: data.find((item) => item.M_Mes === i)?.Q_Proyecc_Monto.toFixed(2) || 0
+                    Q_Proyecc_Monto: data.find((item) => item.M_Mes === i)?.Q_Proyecc_Monto || 0
                 })
             }
             setProjections(dataAllMonths);
@@ -76,61 +76,6 @@ export const RateProjectionsModal = ({
         }
     }
 
-    //   const debounceRef = useRef(null);
-    //   const [dependenciasSelected, setDependenciasSelected] = useState({});
-
-    //   useEffect(() => {
-    //     setDependenciasSelected({});
-    //   }, [show]);
-
-
-    //   const updateTasa = async () => {
-    //     try {
-    //       setIsLoading(true);
-    //       await UpdateTasa({ c_tasa: rateSelected.C_Tasa, dependencia: dependenciasSelected.value });
-    //       handleClose();
-    //       handleBuscar();
-    //     } catch (error) {
-    //       setIsLoading(false);
-    //       Swal.fire({
-    //         icon: "error",
-    //         title: "Error actualizando tasa",
-    //         text: error.response.data.message,
-    //       });
-    //     } finally {
-    //       setIsLoading(false);
-    //     }
-
-    //   };
-
-
-    //   const getDependencias = async (inputValue) => {
-
-    //     if (!inputValue) {
-    //       return [];
-    //     }
-    //     const data = await SelectDependencia({ ano: 2026, field: "NOMBRE", valor: inputValue, solo_activas: 1 });
-    //     const dependencias = data.map(({ c_depend, n_depend_descripcion }) => ({
-    //       value: c_depend,
-    //       label: `${c_depend} - ${n_depend_descripcion}`,
-    //     }));
-    //     return dependencias;
-    //   };
-
-    //   const promiseOptions = (inputValue) => {
-    //     return new Promise((resolve) => {
-    //       if (debounceRef.current) {
-    //         clearTimeout(debounceRef.current);
-    //       }
-    //       debounceRef.current = setTimeout(async () => {
-    //         const data = await getDependencias(inputValue);
-    //         resolve(data);
-    //       }, 1500);
-    //     });
-    //   };
-
-
-
     return (
         <>
             <Modal show={show} onHide={handleClose}>
@@ -152,11 +97,7 @@ export const RateProjectionsModal = ({
                                 <option key={year} value={year}>{year}</option>
                             ))}
                         </select>
-
-
                     </div>
-
-
                     {
                         projections.length > 0 && (
                             <div className="mt-2">
@@ -184,6 +125,10 @@ export const RateProjectionsModal = ({
                             </div>
                         )
                     }
+                    <div>
+                        <h5 className="text-end m-2">Total proyectado: {formatMoney(projections.reduce((acc, curr) => acc + curr.Q_Proyecc_Monto, 0))}</h5>
+                        
+                    </div>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button
@@ -196,7 +141,7 @@ export const RateProjectionsModal = ({
                     <Button
                         variant="primary"
                         onClick={saveProjections}
-                        disabled={isLoading}                    
+                        disabled={isLoading}
                     >
                         {isLoading ? (
                             <Spinner animation="border" size="sm" />
